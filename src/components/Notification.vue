6bec0f061df4686e97eede829363aae65ff00b36
alimirjahani <ali.mirjahani@cafebazaar.ir>
2021-01-28 01:11:17 +0330

    notifs done

---
+++ src/components/Notification.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-if="notif.action==='like'" class="w-full p-2  hover:bg-lighter flex ">
+      <div class="flex-none mr-2">
+        <img :src="`${notif.profile_image}`" class="h-12 w-12 rounded-full flex-none"/>
+      </div>
+      <div>
+        <div>{{ notif.user }} liked your tweet</div>
+        <p class="font-semibold"> {{ notif.tweet }} </p>
+      </div>
+    </div>
+    <div v-if="notif.action==='retweet'" class="w-full p-2  hover:bg-lighter flex ">
+      <div class="flex-none mr-2">
+        <img :src="`${notif.profile_image}`" class="h-12 w-12 rounded-full flex-none"/>
+      </div>
+      <div>
+        <div>{{ notif.user }} retweeted your Tweet</div>
+        <p class="font-semibold"> {{ notif.tweet }} </p>
+      </div>
+    </div>
+    <div v-if="notif.action==='follow'" class="w-full p-2  hover:bg-lighter flex ">
+      <div class="flex mr-2 ">
+        <img :src="`${notif.profile_image}`" class="h-12 w-12 rounded-full flex-none"/>
+      </div>
+      <div class="flex-none mr-2 p-1">
+        <div>{{ notif.user }} followed you </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notification",
+  props: ['notif']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -1,10 +1,59 @@
 <template>
-  <div>hiiiii</div>
+  <div id="home" class="flex container h-screen w-full">
+    <SideNav :tabs="getTabs"></SideNav>
+    <div class="w-full md:w-1/2 h-full">
+      <div class="px-5 py-3 border-b border-lighter flex items-center justify-between">
+        <h1 class="text-xl font-bold">Notifications</h1>
+        <i class="far fa-star text-xl text-blue"></i>
+      </div>
+      <div class="flex flex-col-reverse">
+      </div>
+      <Notification v-for="(notif,index) in notifs" :key="index" :notif="notif"></Notification>
+    </div>
+    <Trending :trending="getTrends"></Trending>
+  </div>
 </template>
 
 <script>
+import Notification from "@/components/Notification";
+import {mapGetters} from "vuex";
+import Trending from "@/components/Trending";
+import SideNav from "@/components/SideNav";
+
 export default {
-  name: "Notification"
+  components: {
+    Notification,
+    Trending,
+    SideNav
+  },
+  data() {
+    return {
+      notifs: [
+        {
+          'profile_image': 'https://pbs.twimg.com/profile_images/1273242022892244997/5RfTzI9z_400x400.jpg',
+          'user': 'mamad',
+          'action': 'like',
+          'tweet': 'my tweet'
+        },
+        {
+          'profile_image': 'https://pbs.twimg.com/profile_images/1273242022892244997/5RfTzI9z_400x400.jpg',
+          'user': 'mamad',
+          'action': 'follow',
+        },
+        {
+          'profile_image': 'https://pbs.twimg.com/profile_images/1273242022892244997/5RfTzI9z_400x400.jpg',
+          'user': 'mamad',
+          'action': 'retweet',
+          'tweet': 'my tweet'
+        },
+      ]
+    }
+  },
+  name: "Notifications",
+  computed: {
+    ...mapGetters([
+      "getTrends", "getTabs"])
+  },
 }
 </script>
 
